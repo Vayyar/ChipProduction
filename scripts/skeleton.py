aac2017a1b7b2622884d6f84d1822b973ac78a67
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 11:52:19 +0300

    [#1] Add method to clean input file from unrelevant lines.

--- scripts/skeleton.py
+++ scripts/skeleton.py
@@ -1,5 +1,6 @@
 import argparse
 
+from typing import List, Set
 
 def parse_file(path_to_read_from: str):
     with open(path_to_read_from, 'r') as input_file:
@@ -7,6 +8,20 @@ def parse_file(path_to_read_from: str):
     chips_map_as_string = clear_from_garbage(file_content)
     chips_map_as_grid: ChipsGrid = ChipsGrid(chips_map_as_string)
     return chips_map_as_grid
+
+
+def clear_from_garbage(chips_map: str) -> str:
+    chips_map_striped: str = chips_map.strip()
+    file_lines: List[str] = chips_map_striped.split('\n')
+    file_lines_striped: List[str] = [line.strip() for line in file_lines]
+    max_not_garbage_line_length = max([len(line) for line in file_lines_striped if is_not_garbage_characters(line)])
+    not_garbage_indexes: Set[int] = {index for index, line in enumerate(file_lines_striped)
+                                     if is_not_garbage(line, max_not_garbage_line_length)}
+    max_lines_continuity_start, max_lines_continuity_end = find_longest_continuous_block_edges(not_garbage_indexes)
+    chips_map_part_list: List[str] = file_lines_striped[max_lines_continuity_start: max_lines_continuity_end + 1]
+    chips_map_part_string: str = '\n'.join(chips_map_part_list)
+    return chips_map_part_string
+
 
 def make_result_text(wafer_grid, neighbors_path: str) -> str:
     pass
