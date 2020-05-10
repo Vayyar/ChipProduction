8f802f010404435d5a58a7e03405f49b4034df69
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 13:42:23 +0300

    [#2] Change neigthbors file name to be field generated in setUp.

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -10,66 +10,61 @@ class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         self.unit_tests_directory: str = '../unit_tests'
+        self.neighbors_filename = 'neighbors_table.json'
 
     def test_middle(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "XXX\n" \
                           "X1X\n" \
                           "XXX"
         expected_output_text: str = "XXX\n" \
                                     "XYX\n" \
                                     "XXX"
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     def test_all_ones(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "111\n" \
                           "111\n" \
                           "111"
         expected_output_text: str = "111\n" \
                                     "111\n" \
                                     "111"
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     def test_dots(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "...\n" \
                           "...\n" \
                           "..."
         expected_output_text: str = "...\n" \
                                     "...\n" \
                                     "..."
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     def test_lonely_one_and_many_dots_bottom(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "...\n" \
                           "...\n" \
                           "1.."
         expected_output_text: str = "...\n" \
                                     "...\n" \
                                     "1.."
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     def test_lonely_one_and_many_dots_top(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "..1\n" \
                           "...\n" \
                           "..."
         expected_output_text: str = "..1\n" \
                                     "...\n" \
                                     "..."
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     def test_all_x(self):
-        neighbors_filename = 'neighbors_table.json'
         input_text: str = "XXX\n" \
                           "XXX\n" \
                           "XXX"
         expected_output_text: str = "XXX\n" \
                                     "XXX\n" \
                                     "XXX"
-        self.a_tester(neighbors_filename, input_text, expected_output_text)
+        self.a_tester(self.neighbors_filename, input_text, expected_output_text)
 
     # Takes 2 seconds
     def test_no_less_xs_no_more_1s_no_more_dots_no_other_chars(self):
