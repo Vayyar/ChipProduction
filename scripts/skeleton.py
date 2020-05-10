da7f2f4457cf7aee1454037cbad9e21e50b98f8e
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 16:11:13 +0300

    [#1] Remove typing .

--- scripts/skeleton.py
+++ scripts/skeleton.py
@@ -1,9 +1,7 @@
 import argparse
 import enum
-import os
 import json
-
-from typing import List, Set, Dict
+import os
 
 
 # TODO I got this error when trying Gooey and I doesn't know what to do
@@ -12,43 +10,44 @@ from typing import List, Set, Dict
 # Horizontal alignment flags are ignored in horizontal sizers
 
 
-def parse_file(path_to_read_from: str):
+def parse_file(path_to_read_from):
     with open(path_to_read_from, 'r') as input_file:
         file_content = input_file.read()
     chips_map_as_string = clear_from_garbage(file_content)
-    chips_map_as_grid: ChipsGrid = ChipsGrid(chips_map_as_string)
+    chips_map_as_grid = ChipsGrid(chips_map_as_string)
     return chips_map_as_grid
 
 
-def clear_from_garbage(chips_map: str) -> str:
-    chips_map_striped: str = chips_map.strip()
-    file_lines: List[str] = chips_map_striped.split('\n')
-    file_lines_striped: List[str] = [line.strip() for line in file_lines]
-    max_not_garbage_line_length = max([len(line) for line in file_lines_striped if is_not_garbage_characters(line)])
-    not_garbage_indexes: Set[int] = {index for index, line in enumerate(file_lines_striped)
-                                     if is_not_garbage(line, max_not_garbage_line_length)}
-    max_lines_continuity_start, max_lines_continuity_end = find_longest_continuous_block_edges(not_garbage_indexes)
-    chips_map_part_list: List[str] = file_lines_striped[max_lines_continuity_start: max_lines_continuity_end + 1]
-    chips_map_part_string: str = '\n'.join(chips_map_part_list)
+def clear_from_garbage(chips_map_as_str):
+    chips_map_striped = chips_map_as_str.strip()
+    file_lines_list = chips_map_striped.split('\n')
+    file_lines_striped_list = [line.strip() for line in file_lines_list]
+    max_not_garbage_line_length = max(
+        [len(line) for line in file_lines_striped_list if is_not_garbage_characters(line)])
+    not_garbage_indexes_set = {index for index, line in enumerate(file_lines_striped_list)
+                               if is_not_garbage(line, max_not_garbage_line_length)}
+    max_lines_continuity_start, max_lines_continuity_end = find_longest_continuous_block_edges(not_garbage_indexes_set)
+    chips_map_part_list = file_lines_striped_list[max_lines_continuity_start: max_lines_continuity_end + 1]
+    chips_map_part_string = '\n'.join(chips_map_part_list)
     return chips_map_part_string
 
 
-def is_not_garbage_characters(line: str) -> bool:
+def is_not_garbage_characters(line):
     """@pre: assume all line are striped
        check that all characters are of chips map
     """
-    relevant_characters: Set[str] = {'X', '.', '1'}
-    is_all_relevant_characters: bool = all([char in relevant_characters for char in line])
-    is_not_garbage_line: bool = is_all_relevant_characters
+    relevant_characters_set = {'X', '.', '1'}
+    is_all_relevant_characters = all([char in relevant_characters_set for char in line])
+    is_not_garbage_line = is_all_relevant_characters
     return is_not_garbage_line
 
 
-def is_not_garbage(line: str, max_not_garbage_line_length: int):
+def is_not_garbage(line, max_not_garbage_line_length):
     """more severe check from 'is_not_garbage_characters' method"""
     return is_not_garbage_characters(line) and len(line) == max_not_garbage_line_length and len(line) > 0
 
 
-def find_longest_continuous_block_edges(set_of_ints: Set[int]):
+def find_longest_continuous_block_edges(set_of_ints):
     max_section_start, max_section_end = -1, -2
     set_of_ints_copy = set_of_ints.copy()
     for number in set_of_ints:
@@ -62,8 +61,8 @@ def find_longest_continuous_block_edges(set_of_ints: Set[int]):
 
 
 def find_edges_of_section_centered_at(number, set_of_ints, to_remove_from):
-    start_index: int = number - 1
-    end_index: int = number + 1
+    start_index = number - 1
+    end_index = number + 1
     while start_index in set_of_ints:
         to_remove_from.remove(start_index)
         start_index -= 1
@@ -77,17 +76,17 @@ def find_edges_of_section_centered_at(number, set_of_ints, to_remove_from):
 
 
 class ChipsGrid:
-    def __init__(self, map_as_string: str):
-        self.__map_as_grid: List[List[Chip]] = ChipsGrid.make_chips_grid(map_as_string)
+    def __init__(self, map_as_string):
+        self.__map_as_grid = ChipsGrid.make_chips_grid(map_as_string)
 
     @staticmethod
-    def make_chips_grid(map_as_string: str):
-        map_as_list: List[str] = map_as_string.split('\n')
-        chips_grid_obj: List[List[Chip]] = list()
+    def make_chips_grid(map_as_string):
+        map_as_list = map_as_string.split('\n')
+        chips_grid_obj = list()
         for row_index, chips_row in enumerate(map_as_list):
             chips_grid_obj.append([])
             for column_index, chip_state in enumerate(chips_row):
-                current_chip: Chip = Chip(row_index, column_index, chip_state)
+                current_chip = Chip(row_index, column_index, chip_state)
                 chips_grid_obj[-1].append(current_chip)
         return chips_grid_obj
 
@@ -106,9 +105,9 @@ class ChipsGrid:
         return len(self.map_as_grid[0])
 
     def __repr__(self):
-        representation_grid: List[List[str]] = [[str(chip) for chip in grid_row] for grid_row in self.map_as_grid]
-        representation_lines: List[str] = [''.join(representation_row) for representation_row in representation_grid]
-        representation: str = '\n'.join(representation_lines)
+        representation_grid = [[str(chip) for chip in grid_row] for grid_row in self.map_as_grid]
+        representation_lines_list = [''.join(representation_row) for representation_row in representation_grid]
+        representation = '\n'.join(representation_lines_list)
         return representation
 
     def __iter__(self):
@@ -122,19 +121,19 @@ class ChipsGrid:
                 if delta_rows == delta_columns == 0:
                     continue
                 elif self.exist_place(chip, delta_rows, delta_columns):
-                    result_row: int = chip.row + delta_rows
-                    result_column: int = chip.column + delta_columns
-                    current_neighbor: Chip = self.map_as_grid[result_row][result_column]
+                    result_row = chip.row + delta_rows
+                    result_column = chip.column + delta_columns
+                    current_neighbor = self.map_as_grid[result_row][result_column]
                     yield current_neighbor
 
     def exist_place(self, chip, delta_rows, delta_columns):
-        result_row: int = chip.row + delta_rows
-        result_column: int = chip.column + delta_columns
-        is_existing_place: bool = 0 <= result_row < self.rows_number and 0 <= result_column < self.columns_number
+        result_row = chip.row + delta_rows
+        result_column = chip.column + delta_columns
+        is_existing_place = 0 <= result_row < self.rows_number and 0 <= result_column < self.columns_number
         return is_existing_place
 
     def number_of_neighbors(self, chip):
-        neighbors_number: int = sum(1 for _ in self.neighbors_iterator(chip))
+        neighbors_number = sum(1 for _ in self.neighbors_iterator(chip))
         return neighbors_number
 
     def number_of_x_neighbors(self, chip):
@@ -149,20 +148,20 @@ class Chip:
         self.__state = Chip.translate_state_from_string_to_enum(state)
 
     @staticmethod
-    def translate_state_from_string_to_enum(state: str):
-        state_translation_dict: Dict[str, ChipState] = {'X': ChipState.Die, '1': ChipState.Live,
-                                                        '.': ChipState.NotAChip, 'Y': ChipState.DieByPrediction}
-        chip_state: ChipState = state_translation_dict[state]
+    def translate_state_from_string_to_enum(state_as_str):
+        state_translation_dict = {'X': ChipState.Die, '1': ChipState.Live,
+                                  '.': ChipState.NotAChip, 'Y': ChipState.DieByPrediction}
+        chip_state = state_translation_dict[state_as_str]
         return chip_state
 
     def __repr__(self):
         return Chip.translate_state_from_enum_to_string(self.state)
 
     @staticmethod
-    def translate_state_from_enum_to_string(chip_state: 'ChipState'):
-        state_translation_dict: Dict[ChipState, str] = {ChipState.Die: 'X', ChipState.Live: '1',
-                                                        ChipState.NotAChip: '.', ChipState.DieByPrediction: 'Y'}
-        state: str = state_translation_dict[chip_state]
+    def translate_state_from_enum_to_string(chip_state_enum):
+        state_translation_dict = {ChipState.Die: 'X', ChipState.Live: '1',
+                                  ChipState.NotAChip: '.', ChipState.DieByPrediction: 'Y'}
+        state = state_translation_dict[chip_state_enum]
         return state
 
     @property
@@ -182,8 +181,8 @@ class Chip:
         self.__state = new_state
 
 
-def make_result_text(wafer_grid: 'ChipsGrid', neighbors_path: str) -> str:
-    neighbors_table: Dict[int, int] = make_dict_of_neighbors_threshold(neighbors_path)
+def make_result_text(wafer_grid, neighbors_path):
+    neighbors_table = make_dict_of_neighbors_threshold(neighbors_path)
     for grid_cell in wafer_grid:
         if grid_cell.state != ChipState.Live:
             continue
@@ -196,19 +195,19 @@ def make_result_text(wafer_grid: 'ChipsGrid', neighbors_path: str) -> str:
     return str(wafer_grid)
 
 
-def make_dict_of_neighbors_threshold(neighbors_path: str):
+def make_dict_of_neighbors_threshold(neighbors_path):
     with open(neighbors_path) as neighbors_json_file:
-        data: Dict[str, int] = json.load(neighbors_json_file)
-    neighbors_dict: Dict[int, int] = {int(key): value for key, value in data.items()}
+        data_dict = json.load(neighbors_json_file)
+    neighbors_dict = {int(key): value for key, value in data_dict.items()}
     return neighbors_dict
 
 
-def save_result_text(result: str, output_path: str):
+def save_result_text(result_text, output_path):
     path_of_directory = os.path.dirname(output_path)
     if not os.path.exists(path_of_directory):
         os.makedirs(path_of_directory)
     with open(output_path, 'w') as output_file:
-        output_file.write(result)
+        output_file.write(result_text)
 
 
 class ChipState(enum.Enum):
