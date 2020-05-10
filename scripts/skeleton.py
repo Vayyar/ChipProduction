87a9d9b1e5740bba32c3ddd79a1caa706194393a
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 11:46:48 +0300

    [#1] Parse the command line arguments.

--- scripts/skeleton.py
+++ scripts/skeleton.py
@@ -1,1 +1,8 @@
+import argparse
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', type=str, help='path for input file.')
+    parser.add_argument('output_path', type=str, help='path for input file.')
+    parser.add_argument('neighbors_path', type=str, help='path for table file.')
+    args = parser.parse_args()
