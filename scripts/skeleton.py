c4ed5ad9754b8874eb7036071d786411aa346cd7
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 11:48:16 +0300

    [#1] Some wishful code that describes the project flow first parse then proccess then save the results.

--- scripts/skeleton.py
+++ scripts/skeleton.py
@@ -1,8 +1,21 @@
 import argparse
 
+
+def parse_file(path_to_read_from: str):
+    pass
+
+def make_result_text(wafer_grid, neighbors_path: str) -> str:
+    pass
+
+def save_result_text(result: str, output_path: str):
+    pass
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_path', type=str, help='path for input file.')
     parser.add_argument('output_path', type=str, help='path for input file.')
     parser.add_argument('neighbors_path', type=str, help='path for table file.')
     args = parser.parse_args()
+    chips_grid = parse_file(args.input_path)
+    processed_text = make_result_text(chips_grid, args.neighbors_path)
+    save_result_text(processed_text, args.output_path)
