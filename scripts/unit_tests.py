865f2f008cfed6e529b522be28e01099c792420d
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 13:04:59 +0300

    [#2] Add tests all 1 remain the same.

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -15,6 +15,16 @@ class UnitTests(unittest.TestCase):
                                     "XXX"
         self.a_tester(neighbors_filename, input_text, expected_output_text)
 
+    def test_all_ones(self):
+        neighbors_filename = 'neighbors_table.json'
+        input_text: str = "111\n" \
+                          "111\n" \
+                          "111"
+        expected_output_text: str = "111\n" \
+                                    "111\n" \
+                                    "111"
+        self.a_tester(neighbors_filename, input_text, expected_output_text)
+
     def calculate_output(self, neighbors_filename: str, input_text: str):
         path_for_neighbors_table: str = os.path.join(self.unit_tests_directory, neighbors_filename)
         input_grid: skeleton.ChipsGrid = skeleton.ChipsGrid(input_text)
