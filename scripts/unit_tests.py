c393dd0a6544e5cc62c3b59417f1637b1030e434
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-10 13:06:48 +0300

    [#2] 2 tests of 1 1 and many dots.

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -41,6 +41,26 @@ class UnitTests(unittest.TestCase):
                                     "..."
         self.a_tester(neighbors_filename, input_text, expected_output_text)
 
+    def test_lonely_one_and_many_dots_bottom(self):
+        neighbors_filename = 'neighbors_table.json'
+        input_text: str = "...\n" \
+                          "...\n" \
+                          "1.."
+        expected_output_text: str = "...\n" \
+                                    "...\n" \
+                                    "1.."
+        self.a_tester(neighbors_filename, input_text, expected_output_text)
+
+    def test_lonely_one_and_many_dots_top(self):
+        neighbors_filename = 'neighbors_table.json'
+        input_text: str = "..1\n" \
+                          "...\n" \
+                          "..."
+        expected_output_text: str = "..1\n" \
+                                    "...\n" \
+                                    "..."
+        self.a_tester(neighbors_filename, input_text, expected_output_text)
+
     def calculate_output(self, neighbors_filename: str, input_text: str):
         path_for_neighbors_table: str = os.path.join(self.unit_tests_directory, neighbors_filename)
         input_grid: skeleton.ChipsGrid = skeleton.ChipsGrid(input_text)
