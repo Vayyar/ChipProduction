5914724e73f99771075eac4600da588669e45c22
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-05-28 09:50:37 +0300

    [#10] Move consistency test to the same file with all the rest of the unnit tests.

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -1,9 +1,10 @@
 import math
+import subprocess
 import unittest
 from pathlib import Path
 from random import choice
 
-from scripts import skeleton
+from scripts import main
 
 
 class UnitTests(unittest.TestCase):
@@ -12,9 +13,50 @@ class UnitTests(unittest.TestCase):
         self.unit_tests_directory = Path(__file__).parents[1].absolute() / 'unit_tests'
         self.neighbors_filename = 'neighbors_table.json'
         self.wafer_initial_states = ['X', '1', '.']
-    ##########################################################################################
-    # ############                   ASK THIS IN THE MEETING                        ##########
-    #          TEST FOR ILLUSTRATE ALGORITHMIC QUESTION                  #
+
+        ##########################################################################################
+        # ############                   ASK THIS IN THE MEETING                        ##########
+        #          TEST FOR ILLUSTRATE ALGORITHMIC QUESTION                  #
+
+    def test_consistency(self):
+        # read last .stdf running result
+        # TODO change to Path(__file__)
+        stdf_file_path = Path('../resources/N6W014_N6W014-19E5_WS_CP1_-40_20200313_110305.stdf')
+        results_path = Path('../unit_tests')
+        neighbors_file_path = Path('../resources/neighbors_table.json')
+        command = ['pythonw', '-u', '--ignore-gooey', f'{Path(__file__).parent / "main.py"}', f'{stdf_file_path}',
+                   f'{results_path}',
+                   f'{neighbors_file_path}']
+        print(command)
+        with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE) as process:
+            process.wait()
+        result_file_path = main.get_output_file_path(results_path, stdf_file_path)
+        with open(result_file_path, 'r') as result_file:
+            result_wafer = result_file.read()
+        # restore the original wafer
+        test_wafer = result_wafer.replace('Y', '1')
+        # make new .txt test file from the original wafer
+        test_file_path = result_file_path.parents[1] / 'resources/test_wafer.txt'
+        with open(test_file_path, 'w') as test_file:
+            test_file.write(test_wafer)
+        # run this python file on the new .txt file
+        output_dir_path = Path(__file__).parents[1] / 'results'
+        command = ['pythonw', '-u', '--ignore-gooey', f'{__file__}', f'{test_file_path}', f'{output_dir_path}',
+                   f'{neighbors_file_path}']
+        print(command)
+        with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE) as process:
+            process.wait()
+            # output, err = process.communicate()
+            # if len(err) > 0:
+            #   logger.error(err)
+            # logger.error(err)
+        output_file_path = main.get_output_file_path(output_dir_path, test_file_path)
+        with open(output_file_path, 'r') as result_from_text_file:
+            result_from_text_file_wafer_str = result_from_text_file.read()
+        # compare them.
+        self.assertEqual(result_from_text_file_wafer_str, result_wafer)
 
     def test_is_y_exchange_its_neighbor_to_y(self):
         input_text = "...\n" \
