8fc62aeccde3675b00b9c9d0ee3159a8a400bde0
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-06-03 08:46:57 +0300

    [#11] Write method for make and save table.

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -14,11 +14,16 @@ def plot_input_and_output(input_grid, output_grid, output_dir, input_file_name):
 def make_table(grid_text):
     rows_text = grid_text.split('\n')
     cells_text = [[char for char in row] for row in rows_text]
+
+def make_and_save_table(grid_text, pdf, title):
+    cells_text = make_grid_of_chars(grid_text)
     colors = [[get_color(char) for char in row] for row in cells_text]
-    fig, ax = plt.subplots()
+    figure, ax = plt.subplots()
+    figure.text(0.05, 0.95, title, size=24)
     ax.axis('tight')
     ax.axis('off')
     ax.table(cellText=cells_text, cellColours=colors, loc='center')
+    pdf.savefig(figure)
 
 
 colors_dict = {'.': "w", "1": "g", "X": "r", "Y": "y"}
