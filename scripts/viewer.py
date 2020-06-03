ff90685aadf3cb62f7bb9e0e904dcfb50fabde04
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-06-03 09:48:18 +0300

    [#11]  Add summary to pdf file.

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -6,9 +6,10 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 def plot_input_and_output(input_grid, output_grid, output_dir, input_file_name):
     with PdfPages(output_dir / f'{input_file_name}_summary.pdf') as pdf:
-        make_summary_file(input_grid, output_grid, output_dir, input_file_name)
-        make_and_save_table(input_grid, pdf, title=f'{short_summary} Wafer before calling the program')
-        make_and_save_table(output_grid, pdf, title=f'Wafer after calling the program')
+        short_summary = make_summary_file(input_grid, output_grid, output_dir, input_file_name)
+        make_and_save_table(input_grid, pdf, title=f'Wafer before calling the program')
+        make_and_save_table(output_grid, pdf, title=f'Wafer after calling the program', additional_text=short_summary)
+        make_text_figure(pdf, short_summary)
 
 
 def find_difference_coordinates(input_grid, output_grid):
@@ -53,14 +54,23 @@ def make_grid_of_chars(grid_text):
     return [[char for char in row] for row in rows_text]
 
 
-def make_and_save_table(grid_text, pdf, title):
+def make_and_save_table(grid_text, pdf, title, additional_text=''):
     cells_text = make_grid_of_chars(grid_text)
     colors = [[get_color(char) for char in row] for row in cells_text]
     figure, ax = plt.subplots()
     figure.text(0.05, 0.95, title, size=24)
     ax.axis('tight')
     ax.axis('off')
+    ax.text(100, 100, additional_text)
     ax.table(cellText=cells_text, cellColours=colors, loc='center')
+    pdf.savefig(figure)
+
+
+def make_text_figure(pdf, text):
+    figure, ax = plt.subplots()
+    figure.text(.1, .1, text, size=20)
+    ax.axis('tight')
+    ax.axis('off')
     pdf.savefig(figure)
 
 
