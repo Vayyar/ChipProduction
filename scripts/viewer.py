ea96390955d467263ea4a386fb392ccf790e9516
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-06-03 08:47:26 +0300

    [#11] New method for make summary file.

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -12,6 +12,28 @@ def plot_input_and_output(input_grid, output_grid, output_dir, input_file_name):
 
 
 def make_table(grid_text):
+
+def make_summary_file(input_grid, output_grid, output_dir, input_file_name):
+    number_of_fail_chips = input_grid.count('X')
+    number_of_pass_chips = input_grid.count('1')
+    number_of_chips = number_of_fail_chips + number_of_pass_chips
+    failed_by_prediction = output_grid.count('Y')
+    pass_at_the_end = output_grid.count('1')
+    fail_at_the_end = output_grid.count('X') + output_grid.count('Y')
+    difference_coordinates = find_difference_coordinates(input_grid, output_grid)
+    short_summary = f'The wafer contains {number_of_chips} chips.\n' \
+                    f'From them {number_of_fail_chips} was failed\n' \
+                    f'And {number_of_pass_chips} was pass.\n' \
+                    f'We mark as fails some more {failed_by_prediction} chips.\n' \
+                    f'Total {fail_at_the_end} was failed.\n' \
+                    f'And {pass_at_the_end} was passed.\n'
+    summary = short_summary + \
+              f'The coordinates of chips we mark as failed even they was pass initially are:' \
+              f' {",".join(difference_coordinates)}'
+    summary_text_file_path = output_dir / f'{input_file_name}_summary.txt'
+    with open(summary_text_file_path, 'w') as summary_text_file:
+        summary_text_file.write(summary)
+    return short_summary
     rows_text = grid_text.split('\n')
     cells_text = [[char for char in row] for row in rows_text]
 
