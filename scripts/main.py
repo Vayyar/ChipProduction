ea306c5696e9521d860f7bf127aecef605498c2c
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-06-10 11:05:18 +0300

    [#14] use relativce paths.

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,6 @@ import copy
 import enum
 import json
 import logging
-import sys
 from collections import Counter
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
@@ -11,8 +10,6 @@ from string import Template
 from gooey import Gooey, GooeyParser
 from pystdf.IO import Parser
 from pystdf.V4 import prr
-
-sys.path.append('..')
 
 logger = logging.getLogger('ChipProductionLogger')
 handler = RotatingFileHandler('ChipProductionLogger.log', maxBytes=100_000, backupCount=1)
@@ -482,7 +479,7 @@ def get_argument():
 
 
 def get_default_paths():
-    project_root = Path('C:\\dev\\ChipProduction')
+    project_root = Path('..')
     default_paths = {'input': project_root / 'resources/example.txt', 'output': project_root / 'results'
         , 'neighbors_table': Path('neighbors_table.json')}
     return default_paths
