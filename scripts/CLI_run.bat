73b003fd8cc80bf5018451e80064fef22ce21c3e
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-06-10 11:24:01 +0300

    [#10] Change bat file to include only relative pasths.

--- scripts/CLI_run.bat
+++ scripts/CLI_run.bat
@@ -1,2 +1,1 @@
-cd C:/dev/ChipProduction/scripts
-python main.py --ignore-gooey C:\dev\ChipProduction\resources\N6W014.0V-24.txt ../results ..\\resources\\neighbors_table.json
+python main.py --ignore-gooey ..\\resources\N6W014.0V-24.txt ../results ..\\resources\\neighbors_table.json
