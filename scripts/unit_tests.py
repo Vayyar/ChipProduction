814ec1f27e44650935cb001d7b97390eded1252c
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-08-18 10:12:15 +0300

    [#37] Change unittests to be compatible wi th the new changes in main.

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -1,9 +1,11 @@
 import math
 import subprocess
 import unittest
+from datetime import datetime
 from pathlib import Path
 from random import choice
 
+import utils
 from scripts import main
 
 
@@ -16,35 +18,36 @@ class UnitTests(unittest.TestCase):
 
     def test_consistency(self):
         # read last .stdf running result
-        main_script_path = Path(__file__).parent / "main.py"
-        stdf_file_path = Path('../resources/N6W014_N6W014-19E5_WS_CP1_-40_20200313_110305.stdf')
-        results_path = Path('../unit_tests')
-        neighbors_file_path = Path('../resources/neighbors_table.json')
+        cwd = Path(__file__).parent
+        main_script_path = cwd / "main.py"
+        stdf_file_path = cwd / '../resources/N6W014_N6W014-19E5_WS_CP1_-40_20200313_110305.stdf'
+        results_path = cwd / f'../unit_tests/consistency_test__Date_{datetime.today().strftime("%Y_%m_%d_%H_%M_%S")}'
+        neighbors_file_path = cwd / '../resources/neighbors_table.json'
+        Path.mkdir(results_path)
         command = ['python', '-u', f'{main_script_path}', '--ignore-gooey', f'{stdf_file_path}',
                    f'{results_path}',
                    f'{neighbors_file_path}']
 
-        with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE) as process:
-            process.wait()
-        result_file_path = main.get_output_file_path(results_path, stdf_file_path)
+        subprocess.run(command, input='File\n', stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, encoding='ascii')
+        result_file_path = utils.get_inner_dir_path(results_path) / main.choose_output_filename(stdf_file_path)
+        utils.wait_for_path_to_exists(result_file_path, maximum_time_to_wait=60)
         with open(result_file_path, 'r') as result_file:
             result_wafer = result_file.read()
         # restore the original wafer
         test_wafer = result_wafer.replace('Y', '1')
         # make new .txt test file from the original wafer
-        test_file_path = result_file_path.parents[1] / 'resources/test_wafer.txt'
+        test_file_path = cwd.parent / 'resources/test_wafer.txt'
         with open(test_file_path, 'w') as test_file:
             test_file.write(test_wafer)
         # run this python file on the new .txt file
-        output_dir_path = Path(__file__).parents[1] / 'results'
+        output_dir_path = cwd.parent / 'results'
         command = ['python', '-u', f'{main_script_path}', '--ignore-gooey', f'{test_file_path}',
                    f'{output_dir_path}', f'{neighbors_file_path}']
-        with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE) as process:
-            process.wait()
-
-        output_file_path = main.get_output_file_path(output_dir_path, test_file_path)
+        subprocess.run(command, input='File\n', stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, encoding='ascii')
+        output_file_path = utils.get_inner_dir_path(results_path) / main.choose_output_filename(stdf_file_path)
+        utils.wait_for_path_to_exists(output_file_path, maximum_time_to_wait=60)
         with open(output_file_path, 'r') as result_from_text_file:
             result_from_text_file_wafer_str = result_from_text_file.read()
         # compare them.
