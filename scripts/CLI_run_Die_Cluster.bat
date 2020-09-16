e23e6b0173b2054caf7819b180710ba72e63c7b5
Chaim Chessrak <chaim.chessrak@vayyar.com>
2020-09-16 17:16:31 +0300

    [#37] Update requirments.

--- scripts/CLI_run_Die_Cluster.bat
+++ scripts/CLI_run_Die_Cluster.bat
@@ -1,2 +1,2 @@
-python main.py --ignore-gooey ..\\resources\N6W014.0V-24.txt ../results ..\\resources\\neighbors_table.json
+python scripts/main.py --ignore-gooey -i_dir False --input_file_path ..\\resources\N6W014.0V-24.txt ../results ..\\resources\\neighbors_table.json
 
